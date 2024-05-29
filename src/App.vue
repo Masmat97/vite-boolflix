3d3805d55527dabc25e3f7f4d43a00b2b99eaab8
Masmat97 <matteo.mascio@libero.it>
2024-05-29 16:22:38 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import axios from 'axios';
+
+export default {
+  components: {
+
+  },
+  props: {
+    
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    getMovies() {
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=1d03bbfe81394b9ece2878065d077b7c&/3/movie/top_rated?language=en-US&page=1").then(risultato => {
+        this.store.movies = risultato.
+        console.log()
+      })
+    }
+  },
+  mounted() {
+
+  },
+}
 </script>
 
 <template>
---
+++ src/data/store.js
@@ -0,0 +1,9 @@
+import  { reactive } from 'vue';
+
+const movie = {
+    movie: [
+
+    ]
+}
+
+const iMieiDati = reactive(oggetto);
